57a01fd185c196d58a88421bf499cb155007cd6e
maayan2j <maayan2j@gmail.com>
2020-04-04 23:40:56 +0300

    Yellow Ball

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // console.log(document)
+const playingField = document.getElementById("playing-field")
+console.log(playingField)
+
+const down = document.getElementById("down")
+console.log(down)
+
+document.getElementById("block").style.backgroundColor = "yellow"
